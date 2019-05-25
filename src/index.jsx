c5996e7026b83e97589bded910bdd777c2bc21d4
Mahpeikar Seyedsaeid Pouya <pouya.mahpeikar@aditro.com>
2019-05-25 17:45:16 +0200

    Works!

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import TextField from '@material-ui/core/TextField';
-import { Col, Row, Button, FormControl } from 'react-bootstrap';
+import { Col, Row, Button, FormControl, Form } from 'react-bootstrap';
 
 
 class Index extends React.Component {
@@ -10,25 +9,31 @@ class Index extends React.Component {
         super()
         this.state = {
             textValue: '',
-            articles: [6, 8, 9, 15, 22, 4, 20, 33, 19, 8, 1, 11, 24],
-            count: 3
+            articles: [],
+            bins: [],
+            binCount: 3,
+            accuracy: 0
         }
     }
 
     addArticle() {
         this.state.articles.push(parseInt(this.state.textValue))
-        this.setState({ textValue: '' })
+        this.setState({
+            textValue: '',
+        })
+        this.nameInput.focus();
     }
 
     updateText(e) {
         this.setState({ textValue: e.target.value })
     }
 
-    distribute() {
-        const art = [...this.state.articles].sort((a, b) => b - a)
+    Pack(_art) {
+        const art = !!_arc ? _arc : [...this.state.articles].sort((a, b) => b - a)
 
+        this.setState({ bins: [] })
         const bins = []
-        for (let i = 0; i < this.state.count; i++) {
+        for (let i = 0; i < this.state.binCount; i++) {
             bins.push([])
         }
         art.forEach(x => {
@@ -36,10 +41,13 @@ class Index extends React.Component {
             bins[lightestBinIndex].push(x)
         })
 
-        console.clear()
-        bins.forEach(x => {
-            console.log(x, x.reduce((a, b) => a + b, 0))
+        this.setState({
+            bins: [...bins]
         })
+
+        return [...bins]
+
+        this.forceUpdate();
     }
 
     getLightestBin(bins) {
@@ -68,58 +76,107 @@ class Index extends React.Component {
         this.forceUpdate();
     }
 
+    handleFormChange(e) {
+        const { name, value } = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
+        const articleStyle = {
+            padding: "3px",
+            marginRight: "4px",
+            marginBottom: "4px",
+            backgroundColor: "#edf3fc",
+            minWidth: "30px",
+            justifyContent: "center",
+            border: "1px solid #dee2e6"
+        }
+
         const articles = this.state.articles.map((x, index) =>
-            <span className="d-inline-flex" key={index} style={{
-                padding: "3px",
-                marginRight: "4px",
-                marginBottom: "4px",
-                backgroundColor: "#edf3fc",
-                minWidth: "30px",
-                justifyContent: "center"
-            }}>{x}</span>
+            <span className="d-inline-flex" key={index} style={articleStyle}>{x}</span>
         )
 
+        const packs = this.state.bins.map((x, i) =>
+            <div key={i} className="w-25 d-inline-flex flex-column mb-4 p-2 justify-content-between m-2 shadow-sm" style={{
+                backgroundColor: "whitesmoke", border: "1px solid #dee2e6"
+            }}>
+                <div className="mb-1">
+                    {x.map((y, i) =>
+                        <span className="d-inline-flex" key={i} style={articleStyle}>{y}</span>
+                    )}
+                </div>
+                <span className="text-center"><b>Vikt: </b>{x.reduce((a, b) => a + b, 0)}</span>
+            </div>
+        )
+
+        const avg = () => {
+            if (this.state.articles.length > 0) {
+                return <span>Avg: {Math.round(this.state.articles.reduce((a, b) => a + b, 0) / this.state.binCount)}</span>
+
+            }
+        }
         return (
             <div className="container-fluid main_container d-flex">
                 <div className="row flex-fill pt-3 pb-3">
-                    <Col sx={3}>
+                    <Col md={3}>
                         <Row className="mb-3">
-                            <Col sx={12}>
+                            <Col md={12}>
+                                <Form>
+                                    <Form.Label>Antal Box:</Form.Label>
+                                    <Form.Control type="number" value={this.state.binCount} name="binCount" onChange={this.handleFormChange.bind(this)} />
+                                </Form>
+                            </Col>
+                        </Row>
+                        <Row className="mb-3">
+                            <Col md={12}>
                                 <Button className="w-100" variant="primary" onClick={this.addRandom.bind(this)}>Lägg till 10 random artiklar</Button>
                             </Col>
                         </Row>
                         <Row className="mb-3">
-                            <Col sx={7}>
+                            <Col md={7}>
                                 <FormControl
+                                    ref={(input) => { this.nameInput = input; }}
                                     id="standard-name"
                                     value={this.state.textValue}
                                     onChange={this.updateText.bind(this)}
                                     margin="normal"
                                 />
                             </Col>
-                            <Col sx={5}>
+                            <Col md={5}>
                                 <Button className="w-100" variant="primary" onClick={this.addArticle.bind(this)}>Lägg till</Button>
                             </Col>
                         </Row>
                         <Row className="mb-3">
-                            <Col sx={12}>
-                                {articles}
+                            <Col md={6}>
+                                Antal: {this.state.articles.length}
+                            </Col>
+                            <Col md={6}>
+                                Totalvikt: {this.state.articles.reduce((a, b) => a + b, 0)}
                             </Col>
                         </Row>
                         <Row className="mb-3">
-                            <Col sx={12}>
+                            <Col md={12}>
                                 <Button className="w-100" variant="danger" onClick={this.clearArticles.bind(this)}>Rensa artiklar</Button>
                             </Col>
                         </Row>
                         <Row className="mb-3">
-                            <Col sx={12}>
-                                <Button variant="success" onClick={this.distribute.bind(this)}>Dela ut</Button>
+                            <Col md={6}>
+                                <Button variant="success" onClick={this.Pack.bind(this)}>Dela ut</Button>
+                            </Col>
+                            <Col md={6} className="align-items-center d-flex">
+                                {avg()}
+                            </Col>
+                        </Row>
+                        <Row className="mb-3">
+                            <Col md={12}>
+                                {articles}
                             </Col>
                         </Row>
                     </Col>
                     <Col md={9}>
-
+                        {packs}
                     </Col>
                 </div>
             </div>
